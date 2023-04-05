bf0fcae0e18509e69f2e3fe155523c3dd2500a29
KarlaG56 <213356@ids.upchiapas.edu.mx>
2023-04-04 20:35:55 -0600

    ADD Login

---
+++ src/componentes/Login/Body.jsx
@@ -0,0 +1,14 @@
+function Body(){
+    return(
+        <div className="container">
+            <div className="container-form">
+                <img src="/img/klassy-logo.png" alt="" className="logo-login" />
+                <form action="">
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
