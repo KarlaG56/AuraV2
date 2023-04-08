8d0a87c23a9458daae22f10132a0aceac22f7e05
KarlaG56 <213356@ids.upchiapas.edu.mx>
2023-04-07 21:00:13 -0600

    ADD Control system

--- src/componentes/controlSystem/Body_Menu.jsx
+++ src/componentes/controlSystem/Body_Menu.jsx
@@ -1,6 +1,27 @@
 function Body_Menu() {
     return (
         <div className="div-container-Menu">
+            <div className="div-container-form-status">
+                <form action="" className="div-form-status">
+                    <h3 className="title-status">Estatus</h3>
+
+                    <div className="div-container-data-status">
+                        <label className="label-status">Seleccionar platillo</label>
+                        <select className="select-status" size="10" required>
+                        </select>
+                    </div>
+
+                    <div className="div-container-data-status">
+                        <div className="buttons-status">
+                            <button className="button-status-active">Activo</button>
+                            <button className="button-status-inactive">Agotado</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+
+
             <div className="div-container-form-Menu">
                 <form action="" className="form-Menu">
                     <h3 className="title-Menu">Menú</h3>
@@ -9,22 +30,50 @@ function Body_Menu() {
                         <input type="text" id="platillo" className="input-Menu" required />
                     </div>
 
+                    <div className="div-form-data-select">
+                        <div>
+                            <label className="label-menu" >Seleccionar categoria</label>
+                            <select className="select-menu">
+                                <option value="BFria">Elige</option>
+                                <option value="BFria">Bebidas frías</option>
+                                <option value="BCalient">Bebidas calientes</option>
+                                <option value="Extra">Extra</option>
+                            </select>
+                        </div>
+                    </div>
+
                     <div className="div-form-data">
                         <label>Precio</label>
-                        <input type="text" id="precioPlatillo" className="input-Menu" required />
+                        <input type="text" id="precioPlatillo" className="input-Menu-precio" required />
                     </div>
 
                     <div className="div-form-data">
                         <label>Imagen</label>
-                        <input type="file" id="imgPlatillo" className="input-Menu"required />
+                        <input type="file" id="imgPlatillo" className="input-Menu" required />
                     </div>
-
 
                     <div className="button-contaner-menu">
                         <button className="button-menu">Registrar</button>
                     </div>
                 </form>
             </div>
+
+            <div className="div-container-form-delete">
+                <form action="" className="div-form-delete">
+                    <h3 className="title-delete">Eliminar</h3>
+
+                    <div className="div-container-data-status">
+                        <label className="label-status">Seleccionar platillo</label>
+                        <select className="select-status" size="10" required>
+                        </select>
+                    </div>
+
+                    <div className="div-container-data-delete">
+                        <button className="button-status-delete">Eliminar</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 }
