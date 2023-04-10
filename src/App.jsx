cdea5ee5609b51da56b7dfbebf4b2755b808eeca
KarlaG56 <213356@ids.upchiapas.edu.mx>
2023-04-09 21:12:32 -0600

    ADD Compra

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Control_Product from "./componentes/Page/Control_Product";
 import AcercaDeNosotros from "./componentes/Page/AcercaDeNosotros";
 import Menu from "./componentes/Page/Menu";
 import Servicios from "./componentes/Page/Servicios";
+import Buy from "./componentes/Page/Buy";
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
         <Route path='/' element={<Inicio />} />
         <Route path="/Iniciar-sesion" element={<Login />} />
         <Route path="/Registro" element={<Register />} />
+        <Route path='/Acerca-de-nosotros' element={<AcercaDeNosotros />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/Servicios" element={<Servicios />} />
+        <Route path="/Compra" element={<Buy />} />
         <Route path="/Control_Sistema/Empleado" element={<ControlSystem />} />
         <Route path="/Control_Sistema/Menu" element={<Control_menu />} />
         <Route path="/Control_Sistema/Producto" element={<Control_Product />} />
-        <Route path='/Acerca-de-nosotros' element={<AcercaDeNosotros />} />
-        <Route path="/Control_Sistema" element={<ControlSystem />} />
-        <Route path="/Menu" element={<Menu />} />
-        <Route path="/Servicios" element={<Servicios />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/componentes/Page/Buy.jsx
@@ -0,0 +1,10 @@
+import Body from "../Buy/Body"
+import "../../assets/styles/Buy.css"
+
+function Buy() {
+    return (
+        <Body />
+    )
+}
+
+export default Buy;
