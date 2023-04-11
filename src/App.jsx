8169c51d7716816082dac46f557f2c661466a1c1
KarlaG56 <213356@ids.upchiapas.edu.mx>
2023-04-11 01:06:23 -0600

    DELETE ALL

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./componentes/Page/Login";
-import Register from "./componentes/Page/Register";
-import Control_menu from "./componentes/Page/control_menu";
-import Control_Product from "./componentes/Page/Control_Product";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path="/Iniciar-sesion" element={<Login />} />
-        <Route path="/Registro" element={<Register />} />
-        <Route path="/Control_Sistema/Menu" element={<Control_menu />} />
-      </Routes>
-    </BrowserRouter>
+   <div></div>
   )
 }
 
--- src/componentes/Page/Login.jsx
+++
@@ -1,9 +0,0 @@
-import Body from "../Login/Body";
-
-function Login(){
-    return(
-        <Body/>
-    )
-}
-
-export default Login;
